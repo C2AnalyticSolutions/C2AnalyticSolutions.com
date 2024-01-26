9e1a76e9cd833196d4caae5ad56e168104ea659d
LuiiVuitton <Luis1134@icloud.com>
2024-01-25 23:26:11 -0500

    added project descriptions

--- website/src/pages/Projects.tsx
+++ website/src/pages/Projects.tsx
@@ -63,14 +63,19 @@ function Projects({
             <div className="col mb-4">
               <div className="card">
                 <img
-                  src={imageSrcPath5}
+                  src={imageSrcPath7}
                   className="card-img-top"
                   alt="Card image cap"
                 />
                 <h2>Pallative Team Metrics Dashboard</h2>
 
                 <ul className="list-group list-group-flush">
-                  <li className="list-group-item">Coming soon!</li>
+                  <li className="list-group-item">
+                    Focuses on the volume and case load distributed among each
+                    health care worker. The quantifiable data provides insight
+                    on the resources needed to allocate in the appropriate areas
+                    for program support.{" "}
+                  </li>
                 </ul>
               </div>
             </div>
@@ -86,9 +91,16 @@ function Projects({
                 <h2>C2 Analytic Solutions Website</h2>
 
                 <ul className="list-group list-group-flush">
-                  <li className="list-group-item">AWS S3 Bucket</li>
-                  <li className="list-group-item">AWS Route 53</li>
-                  <li className="list-group-item">AWS CloudFront</li>
+                  <li className="list-group-item">
+                    Designed and developed a fully responsive website using
+                    React Typescript. Our team maintained a Git workflow, using
+                    branches for new features. Successfully deployed our website
+                    to AWS S3 bucket. Used AWS Route 53 to set up our domain
+                    from google. Implemented AWS CloudFront to improve
+                    performance and security of the website. Our team is
+                    providing ongoing support to the website, adding new
+                    features, and maintaining the software.{" "}
+                  </li>
                 </ul>
               </div>
             </div>
@@ -97,14 +109,21 @@ function Projects({
             <div className="col mb-4">
               <div className="card">
                 <img
-                  src={imageSrcPath7}
+                  src={imageSrcPath5}
                   className="card-img-top"
                   alt="Card image cap"
                 />
                 <h2>Palliative Services Line Metrics</h2>
 
                 <ul className="list-group list-group-flush">
-                  <li className="list-group-item">Coming soon!</li>
+                  <li className="list-group-item">
+                    The service line dashboard was designed to provide
+                    visualizations for key performance indicators(KPIs) of the
+                    hospital system’s palliative program. The KPIs provides
+                    insight that showcases how the program’s intervention
+                    improves the overall hospital length of stay in the
+                    palliative population that are seen in the hospital.{" "}
+                  </li>
                 </ul>
               </div>
             </div>
